547ebd4d5259da8076b6ce6b1d919db9cc4c6037
Roman <rprikhoda@ukr.net>
2023-03-27 01:50:41 +0300

    change units

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import { fetchCityWeather, fetchIp } from './js/fetchCityWeather';
+import { Weather, Ip } from './js/fetchCityWeather.js';
 import weatherTemplate from './templates/all-day.hbs';
 import cityListTemplate from './templates/city-list.hbs';
 import getRsfs from './js/refs';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-// const Handlebars = require('parcel-transformer-hbs');
-// import hbs from 'handlebars-inline-precompile';
+const Handlebars = require('handlebars');
 
 const refs = getRsfs();
 
@@ -12,6 +11,10 @@ window.addEventListener('DOMContentLoaded', handleLocation);
 
 refs.form.addEventListener('submit', handleSubmitForm);
 refs.cityList.addEventListener('click', handleButtonClickCityList);
+refs.temprBtn.addEventListener('click', handleClickTempButton);
+
+const weatherForecast = new Weather();
+const getCityFromIp = new Ip();
 
 Notify.init({
   width: '300px',
@@ -34,9 +37,15 @@ Notify.init({
 
 function handleLocation() {
   const cityFromLocalStorage = localStorage.getItem('weathercity');
+  if (!getTempUnit()) {
+    setTempUnit('metric');
+  }
+
+  renderTempButton();
 
   if (!cityFromLocalStorage) {
-    fetchIp()
+    getCityFromIp
+      .fetchIp()
       .then(response => {
         const cityArray = [];
         cityArray.push(changeFirstLetterToUpperCase(response.city));
@@ -62,30 +71,76 @@ function handleSubmitForm(e) {
 
   const cityName = value.trim();
   getCityWeather(cityName);
-
   e.target.elements.query.value = '';
 }
 
 function getCityWeather(city) {
-  fetchCityWeather(city)
+  weatherForecast
+    .fetchCityWeather(city, (unit = getTempUnit()))
     .then(weather => {
-      // console.log(weather);
+      console.log(weather);
       renderWeathertemplate(weather);
       setCityToLocalStorage(changeFirstLetterToUpperCase(city));
+      sessionStorage.setItem('citynow', city);
     })
-    .catch(err => {
+    .catch(() => {
       Notify.failure(
         "We don't have weather from other planets. Try another city :)"
       );
     });
 }
 
-// Handlebars.registerHelper('loud', function (weather) {
-//   weather.main.temp_min.toFixed();
+function handleClickTempButton() {
+  toggleTempUnit();
+  renderTempButton();
+  getCityWeather(sessionStorage.getItem('citynow'));
+}
+
+function getTempUnit() {
+  return localStorage.getItem('weatherunit');
+}
+
+function setTempUnit(unit) {
+  localStorage.setItem('weatherunit', unit);
+}
+
+function toggleTempUnit() {
+  const tempUnit = getTempUnit();
+  const unit = tempUnit === 'metric' ? 'imperial' : 'metric';
+  localStorage.setItem('weatherunit', unit);
+}
+
+function renderTempButton() {
+  const textBtn = getTempUnit();
+  refs.temprBtn.textContent = textBtn === 'metric' ? '\u2109' : '\u2103';
+  // F - '\u2109'
+  // C - '\u2103'
+}
+
+Handlebars.registerHelper('mintemp', function (min) {
+  let value = min.toFixed();
+  const unit = getTempUnit();
+  // console.log((value += unit === 'metric' ? '\u2109' : '\u2103'));
+  return (value += unit === 'metric' ? '\u2103' : '\u2109');
+});
+
+Handlebars.registerHelper('maxtemp', function (max) {
+  let value = max.toFixed();
+  const unit = getTempUnit();
+  return (value += unit === 'metric' ? '\u2103' : '\u2109');
+});
+
+Handlebars.registerHelper('temp', function (temp) {
+  let value = temp.toFixed();
+  const unit = getTempUnit();
+  return (value += unit === 'metric' ? '\u2103' : '\u2109');
+});
+
+// Handlebars.registerHelper('unit', function (unit) {
+//   return unit = unit;
 // });
 
 function renderWeathertemplate(weather) {
-  // weather.main.temp_min.toFixed();
   refs.page.innerHTML = weatherTemplate(weather);
 }
 
--- src/js/fetchCityWeather.js
+++ src/js/fetchCityWeather.js
@@ -1,32 +1,62 @@
-const API = 'e1868383596238a03a9fcf1a7f12bf30';
-const BASE_URL = 'https://api.openweathermap.org/data/2.5/';
-const UNITS = 'metric';
-const COUNTRY = 'ua';
-// faringate units=imperial
+// const API = 'e1868383596238a03a9fcf1a7f12bf30';
+// const BASE_URL = 'https://api.openweathermap.org/data/2.5/';
+// const UNITS = localStorage.getItem('weatherunit') || 'metric';
+// const COUNTRY = 'ua';
+// faringate units=imperial metric
 // const options = {
 //     aplications: {
 //         header: '',
 //     }
 // }
-//https://api.openweathermap.org/data/2.5/weather?q=London&appid=e1868383596238a03a9fcf1a7f12bf30
 
-export function fetchCityWeather(cityName) {
-  return fetch(
-    `${BASE_URL}weather?q=${cityName}&appid=${API}&lang=${COUNTRY}&units=${UNITS}`
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
+export class Weather {
+  #BASE_URL = 'https://api.openweathermap.org/data/2.5/';
+  #API = 'e1868383596238a03a9fcf1a7f12bf30';
 
-    return response.json();
-  });
+  fetchCityWeather(cityName, unit) {
+    return fetch(
+      `${this.#BASE_URL}weather?q=${cityName}&appid=${this.#API}&units=${unit}`
+    ).then(res => {
+      if (!res.ok) {
+        throw new Error(res.status);
+      }
+
+      return res.json();
+    });
+  }
 }
 
-export function fetchIp() {
-  return fetch('https://ipinfo.io/json?token=8851da7fd8c9f0').then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+export class Ip {
+  #BASE_URL = 'https://ipinfo.io';
+  #API = '8851da7fd8c9f0';
+
+  fetchIp() {
+    return fetch(`${this.#BASE_URL}/json?token=${this.#API}`).then(res => {
+      if (!res.ok) {
+        throw new Error(res.status);
+      }
+      return res.json();
+    });
+  }
 }
+
+// export function fetchCityWeather(cityName) {
+//   return fetch(
+//     `${BASE_URL}weather?q=${cityName}&appid=${API}&lang=${COUNTRY}&units=${UNITS}`
+//   ).then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+
+//     return response.json();
+//   });
+// }
+
+// export function fetchIp() {
+//   return fetch('https://ipinfo.io/json?token=8851da7fd8c9f0').then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+//     return response.json();
+//   });
+// }
